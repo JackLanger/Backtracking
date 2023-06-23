2ff60cdae33faa87e1760fdddf9ef17ed4e61ac7
JackL <jacek.langer@outlook.de>
2023-06-23 09:11:10 +0200

    chore: removed dead code

--- src/oszimt/backtrack/Main.java
+++ src/oszimt/backtrack/Main.java
@@ -3,81 +3,76 @@ package oszimt.backtrack;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        int[][] result = damenProblem(8);
+  public static void main(String[] args) {
 
-        System.out.println(Arrays.deepToString(result));
-        // todo: create chess boards.
-    }
+    int[][] result = damenProblem(8);
 
-    public static int[][] damenProblem(int size) {
-        //TODO: refactor, remove tags and simplify
-        int[][] res = new int[size][];
-        boolean[] isRevistedZeros = new boolean[size];
-        mainLoop:
+    System.out.println(Arrays.deepToString(result));
+    // todo: create chess boards.
+  }
 
-        for (int a = 0; a < size; a++) {
 
-            int[] path = new int[size];
-            path[0] = a;
-            outer:
-            for (int i = 1; i < size; i++) {
+  public static int[][] damenProblem(int size) {
+    //TODO: refactor, remove tags and simplify
+    int[][] res = new int[size][];
+    boolean[] isRevistedZeros = new boolean[size];
+    mainLoop:
 
-                if (i < size - 1) {
-                    path[i + 1] = 0;
-                    isRevistedZeros[i+1] = false;
-                }
+    for (int a = 0; a < size; a++) {
 
-                for (int j = 0; j < size; j++) {
+      int[] path = new int[size];
+      path[0] = a;
+      outer:
+      for (int i = 1; i < size; i++) {
 
-                    if (isValidPosition(path, i, j, isRevistedZeros[i])) {
-                        path[i] = j;
-                        continue outer;
-
-                    } else if (j == size - 1) {
-
-                        if (i <= 1) {
-                            // if first index is already at 7 we do not have an answer.
-                            if (path[0] == size - 1)
-
-                                if (j == i) {
-                                    continue mainLoop;
-
-                                } else path[0]++;
-
-                        } else {
-                            isRevistedZeros[i-1] = true;
-                            i -= 2;// reduce i to step back
-                        }
-                    }
-                }
-
-            }
-            res[a] = path;
+        if (i < size - 1) {
+          path[i + 1] = 0;
+          isRevistedZeros[i + 1] = false;
         }
 
-        return res;
-    }
+        for (int j = 0; j < size; j++) {
 
-    private static boolean isValidPosition(int[] path, int i, int j, boolean isRevistedZero) {
-        // check horizontal
+          if (isValidPosition(path, i, j, isRevistedZeros[i])) {
+            path[i] = j;
+            continue outer;
 
-        for (int k = 0; k < i; k++) {
-            if (path[k] == j) return false;
+          } else if (j == size - 1) {
+
+            isRevistedZeros[i - 1] = true;
+            i -= 2;// reduce i to step back
+
+          }
         }
 
-        // check if the placement is not a neighbor of any other node,
-        // check if it is not diagonal to any other node, choose the smallest possible value,
-        // that is bigger than the previous value. allow 0 !
-        return isNotInDiagonal(path, j, i) && (j > path[i] || (path[0] != 0 && j == 0 && !isRevistedZero));
+      }
+      res[a] = path;
     }
 
-    private static boolean isNotInDiagonal(int[] path, int j, int currentIndex) {
-        for (int k = 0; k < currentIndex; k++) {
-            if (path[k] == j - (k - currentIndex) || path[k] == j + (k - currentIndex))
-                return false;
-        }
-        return true;
+    return res;
+  }
+
+
+  private static boolean isValidPosition(int[] path, int i, int j, boolean isRevistedZero) {
+    // check horizontal
+
+    for (int k = 0; k < i; k++) {
+      if (path[k] == j) return false;
     }
+
+    // check if the placement is not a neighbor of any other node,
+    // check if it is not diagonal to any other node, choose the smallest possible value,
+    // that is bigger than the previous value. allow 0 !
+    return isNotInDiagonal(path, j, i) && (j > path[i] || (path[0] != 0 && j == 0 && !isRevistedZero));
+  }
+
+
+  private static boolean isNotInDiagonal(int[] path, int j, int currentIndex) {
+
+    for (int k = 0; k < currentIndex; k++) {
+      if (path[k] == j - (k - currentIndex) || path[k] == j + (k - currentIndex))
+        return false;
+    }
+    return true;
+  }
 
 }
